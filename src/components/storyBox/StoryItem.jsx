d7c3147c0d6bb85849a9cf1afe4d81eeeb9d96d3
Bao Minh <haphanabominh9674@gmail.com>
2023-01-29 14:06:16 +0700

    updated avatar

--- src/components/storyBox/StoryItem.jsx
+++ src/components/storyBox/StoryItem.jsx
@@ -3,7 +3,7 @@ import srcDemo from "../../images/images.jfif"
 
 function StoryItem({ width }) {
     return (
-        <div className={`w-[${width}] h-[${width}] border-red-400 border-[2px] p-[2px] cursor-pointer rounded-full flex justify-center items-center`}>
+        <div className={`w-[58px] h-[58px] border-red-400 border-[2px] p-[2px] cursor-pointer rounded-full flex justify-center items-center`}>
             <img loading="lazy" src={srcDemo} alt="" className='w-full h-full object-cover rounded-full' />
         </div>
     )
